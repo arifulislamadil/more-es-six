886adee04a8cbab0b1367e88d6ed4b8cb79b0e52
Arifu Islam Adil <adil.islam.arif@gmail.com>
2022-01-03 15:56:03 +0600

    find filter

--- filter-find.js
+++ filter-find.js
@@ -11,6 +11,7 @@ const products = [
     { name: 'Aunglass', price: 300, brand: 'Ribon', color: 'Blue' },
     { name: 'Camera', price: 9000, brand: 'Lenovo', color: 'Gray' },
 ];
-const bigNumbers = products.filter(product => product.price > 4000);
+// const bigNumbers = products.filter(product => product.price > 4000);
+const bigNumbers = products.find(product => product.price > 4000);
 console.log(bigNumbers);
 //Output: [30]
